ab35aec37c08663bdca3ceeec0cdc2955c619626
Ubuntu <ubuntu@ip-172-31-77-108.ec2.internal>
2021-06-18 14:33:26 +0000

    added

---
+++ add_two_number.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Created by: Sam. Garcon
+# Created on: June 2021
+# This program shows how to adds the 2 number together
+
+ # input
+number1 = int(input("Enter the first number to add (integer): "))
+number2 =int(input("Enter the second number to add (integer): "))
+
+  # process
+result= number1 + number2
+
+print(number1," + ",number2," = ",result)
+print("\nDone.")
